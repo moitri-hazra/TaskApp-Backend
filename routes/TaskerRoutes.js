0c6e242b1140220aa97192c382e0394bf7fb7c60
moitri-hazra <moitrihazra2000@gmail.com>
2024-02-22 19:00:50 +0530

    deleted

--- routes/TaskerRoutes.js
+++ routes/TaskerRoutes.js
@@ -1,11 +1,7 @@
 const {Router} = require('express');
 const { getTask, saveTask, updateTask, deleteTask, singleTask } = require('../controllers/TaskerController');
-const authController = require('../controllers/UserController');
 
 const router = Router();
-
-router.post('/register', authController.registerUser);
-router.post('/login', authController.loginUser);
 
 router.get('/', getTask)
 router.get('/task/:id', singleTask);
